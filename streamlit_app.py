d87ad9bb1e8a48c84d2c4ad2bb6a6d1b4efd6cf5
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 08:23:38 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,10 @@ import streamlit_app2
 
 import streamlit as st
 PAGES = {
-    "App1": streamlit_app1,
-    "App2": streamlit_app2
+    "Black Lives Matter": streamlit_app1,
+    "Home": streamlit_app2
 }
 st.sidebar.title('Navigation')
 selection = st.sidebar.radio("Go to", list(PAGES.keys()))
 page = PAGES[selection]
+page.app()

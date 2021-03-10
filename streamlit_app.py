fb282cf088105710317622dfd39231dd8b83fd8b
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 12:45:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ import streamlit_app2
 import streamlit as st
 PAGES = {
      "Home": streamlit_app2,
-    "Black Lives Matter": streamlit_app1
+    "Black Lives Matter": streamlit_app1,
+     "Gun Violence": 
    
 }
 st.sidebar.title('Navigation')

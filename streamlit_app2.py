7bd28493ffe0e2f7b83dbd8e837dbe32e2086263
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 08:06:37 -0600

    Create streamlit_app2.py

---
+++ streamlit_app2.py
@@ -0,0 +1,13 @@
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+
+import datetime
+import numpy as np
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
+
+
+st.button(Gun Violence, key=None)

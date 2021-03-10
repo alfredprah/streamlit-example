de401ae6559d021dcb6da9e099cabb523a30d85e
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 00:06:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import streamlit as st
 """
 # Welcome to the c4project Dashboard!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+This is our very first sample dashboard to explore the possibilities of using Tweets to gauge the different trajectories of projests:
 
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).

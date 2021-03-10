c862ecf65446c427a15cd67ab1ac4af13463a70b
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 08:27:44 -0600

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -11,3 +11,8 @@ import matplotlib.pyplot as plt
 
 def app():
   st.button('Gun Violence', key=None)
+  st.button('Refugee Crisis', key=None)
+  st.button('Healthcare', key=None)
+  st.button('Voting Rights', key=None)
+  st.button('Income Gap', key=None)
+  st.button('Black Lives Matter', key=None)

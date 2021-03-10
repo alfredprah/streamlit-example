df51b62e9230dc350eec99358664e18c57d757b9
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 08:09:39 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+#app.py
+import streamlit_app1
+import streamlit_app2
+
+import streamlit as st
+PAGES = {
+    "App1": streamlit_app1,
+    "App2": streamlit_app2
+}
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()

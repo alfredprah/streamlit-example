1c52b6ae97c3102b31ba44774b08b83aab672fcb
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 00:53:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,9 @@ import matplotlib.pyplot as plt
 """
 
 data = pd.read_csv('result.csv')
+
 st.dataframe(data, width=None, height=None)
+
 total_points = st.slider("Number of points in tweets", 1, 5000, 200)
 num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 Point = namedtuple('Point', 'x y')

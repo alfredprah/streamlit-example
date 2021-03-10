daaf80801a642acdf76b6ae4009eb4e280785d69
Alfred Prah <31881173+alfredprah@users.noreply.github.com>
2021-03-10 12:49:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,12 +5,13 @@ import streamlit_app2
 import streamlit as st
 PAGES = {
      "Home": streamlit_app2,
-     "Black Lives Matter": streamlit_app1,
      "Gun Violence": streamlit_app1,
      "Refugee Crisis":streamlit_app1,
-     "Income Gap":streamlit_app1,
      "Healthcare":streamlit_app1,
-     "Voting Rights":streamlit_app1
+     "Voting Rights":streamlit_app1,
+     "Income Gap":streamlit_app1,
+     "Black Lives Matter": streamlit_app1
+     
    
 }
 st.sidebar.title('Navigation')
